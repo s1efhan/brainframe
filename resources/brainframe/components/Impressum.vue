e761a4e852b49584af57341e76bf8145dbfa2cc0
s1efhahn <128990558+s1efhan@users.noreply.github.com>
2024-11-20 11:47:07 +0100

    Update Impressum.vue

--- resources/brainframe/components/Impressum.vue
+++ resources/brainframe/components/Impressum.vue
@@ -4,9 +4,8 @@
 <section class="impressum">
 <h3>Nach § 5 TMG</h3>
     <p>Stefan Theißen</p>
-    <p>c/o Impressumservice Dein-Impressum</p>
-    <p>Stettiner Straße 41</p>
-    <p>35410 Hungen</p>
+    <p>In den Kronprinzengärten 15</p>
+    <p>52066 Aachen</p>
     <p> Telefon: 004915252812838</p>
     <p>Email: theissen-stefan@mail.de</p>
 </section>
